b9b98a1f0e8d23ca50f15a8ed5ffde3c9662124b
Harish <harish@Harishs-MacBook-Air.local>
2025-11-21 23:00:07 +0530

    Update: new doctypes added

---
+++ brihas_harish/brihas_harish/doctype/camera/test_camera.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Harish Ankalla and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCamera(FrappeTestCase):
+	pass
